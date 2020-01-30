3a1af4d592cd5b96386d974839f45cf8149b15d2
Jonathan Zhu <jhzhuustc@gmail.com>
2020-01-30 15:57:42 +0800

    refactor: 第三方库的 cdn 都切换到 cdnjs 上去

--- src/template.user.js
+++ src/template.user.js
@@ -9,9 +9,9 @@
 // @require      https://openuserjs.org/src/libs/sizzle/GM_config.js
 // @require      http://code.jquery.com/jquery-3.4.1.slim.min.js
 // @require      https://gmousse.github.io/dataframe-js/dist/dataframe.min.js
-// @require      https://unpkg.com/dexie@latest/dist/dexie.js
-// @require      https://momentjs.com/downloads/moment.js
-// @require      https://unpkg.com/xlsx/dist/xlsx.full.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/dexie/2.0.4/dexie.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.15.5/xlsx.full.min.js
 // @require      http://res.bookbook.pub/js/later/1.2.0/later.min.js
 // @grant             unsafeWindow
 // @grant             GM_xmlhttpRequest
